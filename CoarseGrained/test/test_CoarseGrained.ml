232d2599b1b2410aa93c0297a75f54d1a36697ce
Stephanos <s.faustmann@student.rug.nl>
2024-06-11 21:41:34 +0200

    Dune testing

--- CoarseGrained/test/test_CoarseGrained.ml
+++ CoarseGrained/test/test_CoarseGrained.ml
@@ -1,26 +1,45 @@
 open Approach.CoarseGrained
-(* Test function to initialize and manipulate the linked list *)
-(* let testaddition () =
+(* Function to test basic linked list operations *)
+let test_operations () =
   let linkedlist = create_linkedlist () in
-  let node1 = {value = min_int; key = Hashtbl.hash min_int; next = None} in
-  linkedlist.firstnode <- Some node1;
-  ignore (additem linkedlist max_int);  
-  ignore (additem linkedlist 5);
-  ignore (additem linkedlist 8);
-  ignore (additem linkedlist 5);
-  print_list linkedlist.firstnode;
-  ignore (removeitem linkedlist 5);
-  print_list linkedlist  Print the list *)
+  assert (additem linkedlist 1);
+  assert (additem linkedlist 2);
+  assert (additem linkedlist 3);
+  assert (contains linkedlist 1);
+  assert (contains linkedlist 2);
+  assert (contains linkedlist 3);
+  assert (removeitem linkedlist 2);
+  assert (not (contains linkedlist 2));
+  Printf.printf "Basic operations test passed\n"
 
-(* Test parallel operations on the list *)
+(* Function to test barrier synchronization *)
+let test_barrier () =
+  let barrier = create_barrier 2 in
+  let counter = Atomic.make 0 in
+  let domainA = Domain.spawn (fun () ->
+    await barrier;
+    Atomic.incr counter;
+    Printf.printf "Domain A passed the barrier\n"
+  ) in
+  let domainB = Domain.spawn (fun () ->
+    await barrier;
+    Atomic.incr counter;
+    Printf.printf "Domain B passed the barrier\n"
+  ) in
+  Domain.join domainA;
+  Domain.join domainB;
+  assert (Atomic.get counter = 2);
+  Printf.printf "Barrier synchronization test passed\n"
+
+(* Function to test parallel operations *)
 let test_parallel () =
   let linkedlist = create_linkedlist () in
   let barrier = create_barrier 2 in
   let domainA = Domain.spawn (fun () ->
     await barrier;
-    ignore (additem linkedlist 1 );
-    ignore (additem linkedlist 2 );
-    ignore (additem linkedlist 3 );
+    ignore (additem linkedlist 1);
+    ignore (additem linkedlist 2);
+    ignore (additem linkedlist 3);
     ignore (removeitem linkedlist 20);
     ignore (removeitem linkedlist 4);
     ignore (removeitem linkedlist 5);
@@ -29,16 +48,21 @@ let test_parallel () =
   ) in
   let domainB = Domain.spawn (fun () ->
     await barrier;
-    ignore (additem linkedlist 4 );
-    ignore (additem linkedlist 5 );
-    ignore (additem linkedlist 6 );
+    ignore (additem linkedlist 4);
+    ignore (additem linkedlist 5);
+    ignore (additem linkedlist 6);
     ignore (removeitem linkedlist 15);
     ignore (additem linkedlist 8);
-    ignore (removeitem linkedlist 3 );
-    ignore (removeitem linkedlist 2 )
+    ignore (removeitem linkedlist 3);
+    ignore (removeitem linkedlist 2)
   ) in
   Domain.join domainA;
   Domain.join domainB;
-  print_list linkedlist
+  print_list linkedlist;
+  Printf.printf "Parallel operations test passed\n"
 
-let () = test_parallel ()
+(* Main function to run all tests *)
+let () =
+  test_operations ();
+  test_barrier ();
+  test_parallel ()
